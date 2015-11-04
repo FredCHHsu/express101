3e395c9f45cc2fb9ce115706d1506c8c8cd2961f
fred.c.h.hsu <fred.c.h.hsu@gmail.com>
2015-11-05 01:54:20 +0800

    Using Jade and Add Basic Views

--- index.js
+++ index.js
@@ -2,19 +2,29 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'jade');
+
 app.set('port', process.env.PORT || 3000);
 
-app.use(function(req, res){
-  res.type('text/plain');
+app.get('/', function(req, res){
+  // res.type('text/plain');
+  // res.send('Meadowlark Travel');
+  res.render('home');
+});
+
+app.get('/about', function(req, res){
+  res.render('about');
+});
+
+app.use(function(req, res, next){
   res.status(404);
-  res.send('404 - not found');
+  res.render('404');
 });
 
 app.use(function(err, req, res, next){
   console.error(err.stack);
-  res.type('text/plain');
   res.status(500);
-  res.send('500 - Server Error');
+  res.render('500');
 });
 
 app.listen(app.get('port'), function(){
